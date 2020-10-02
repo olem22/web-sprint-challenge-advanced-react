2cde75cb5772113e7ae98e191f080d490c0b58a1
unknown <olem2233@gmail.com>
2020-10-02 10:36:01 -0700

    Still working with testing

--- client/src/components/CheckoutForm.js
+++ client/src/components/CheckoutForm.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { useForm } from '../hooks/useForm'
 
 const initialValue = {
   firstName: "",
@@ -15,11 +16,11 @@ const initialValue = {
 
 const CheckoutForm = (props) => {
   const [showSuccessMessage, setShowSuccessMessage] = useState(false);
-  const [values, setValues] = useState(initialValue);
+  const [values, handleChanges] = useForm("signupForm",initialValue);
 
-  const handleChanges = (e) => {
-    setValues({ ...values, [e.target.name]: e.target.value });
-  };
+ 
+
+
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -72,7 +73,7 @@ const CheckoutForm = (props) => {
       {showSuccessMessage && (
         <div className="success-message" data-testid="successMessage">
           <p>
-            You have ordered some plants! Woo-hoo! <span role="img">🎉</span>
+            You have ordered some plants! Woo-hoo! <span role="img" aria-label="gift">🎉</span>
           </p>
           <p>Your new green friends will be shipped to:</p>
           <br />
